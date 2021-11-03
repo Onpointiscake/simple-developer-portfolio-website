3e8a62318c2acdf2bd8620f75ecd27e830356ecb
Javier Suarez <45120507+Onpointiscake@users.noreply.github.com>
2021-11-03 11:01:09 +0100

    hide myfavouriteprojects component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
-import FavouriteProjects from "../components/FavouriteProjects";
+//import FavouriteProjects from "../components/FavouriteProjects";
 import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
@@ -14,7 +14,6 @@ export default function Home({ repositories }) {
       description="Welcome. This is my personal portfolio."
     >
       <Hero />
-      <FavouriteProjects />
       <LatestCode repositories={repositories} />
     </ContainerBlock>
   );
